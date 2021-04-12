566ee92ce95319979e169ffad4302ccf17133850
Yoann <80095031+YoannPierkot@users.noreply.github.com>
2021-04-12 14:00:06 +0200

    ajout du script campagne

--- MythsAndSteel/Assets/MetaGame/Scripts/Menu/MenuButton.cs
+++ MythsAndSteel/Assets/MetaGame/Scripts/Menu/MenuButton.cs
@@ -1,30 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class MenuButton : MonoBehaviour
 {
-    [SerializeField] int SceneToLoad;
-    [SerializeField] GameObject CanvasToLoad;
-    [SerializeField] GameObject CanvasParent;
-    public void ChangeScene()
-    {
-        SceneManager.LoadScene(SceneToLoad);
-    }
+    
+    [SerializeField] GameObject CanvasToLoad; //Le Canvas que l'on veuet monter 
+    [SerializeField] GameObject CanvasParent;//Le Canvas dans lequel se trouve l'objet, qui sera désafficher
+    [SerializeField] bool CanReturn;//Est ce que on peut revenir au Canvas précédent?
 
     public void QuitGame()
     {
         Application.Quit();
     }
 
-    public void LoadCanvas()
+    public void LoadCanvas() //permet de charger un Canvas
     {
         CanvasToLoad.SetActive(true);
         CanvasParent.SetActive(false);
-        
     }
 
-    
+    public void ReturnsCanvas()//permet de revenir au Canvas précédent
+    {
+        CanvasParent.SetActive(false);
+        CanvasToLoad.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (CanReturn & Input.GetKey(KeyCode.Escape))// Retour arrière avec la touche (échappe si possible)
+        {
+            ReturnsCanvas();
+        }
+    }
+
 }
     
