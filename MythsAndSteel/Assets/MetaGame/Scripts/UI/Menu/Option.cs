dc3dc89d65af5413bb0fb811a4aa37ab65c9fd4a
TheoooooM <t.marcuzzi@rubika-edu.com>
2021-04-22 01:58:12 +0200

    Validation + comment

--- MythsAndSteel/Assets/MetaGame/Scripts/UI/Menu/Option.cs
+++ MythsAndSteel/Assets/MetaGame/Scripts/UI/Menu/Option.cs
@@ -7,11 +7,17 @@ using UnityEngine.UI;
 public class Option : MonoBehaviour
 {
     //---------Audio--------------
-    public AudioMixer audioMixer;
+    public AudioMixer audioMixer; //Variable qui défini quel AudioMixer on modifie
 
     //--------Résolution----------
-    public Dropdown ResolutionDropdown;
-    Resolution[] resolutions;
+    public Dropdown ResolutionDropdown;//Variable qui défini quel dropdown on modifie
+    Resolution[] resolutions;//Array de toute 
+    int currentResolutionIndex = 0;
+    Resolution OldResolution;
+
+    bool FirstTime;
+
+    [SerializeField] GameObject ValidationPanel;
 
     //-------Avertissement--------
     bool isAvertissement;
@@ -19,33 +25,61 @@ public class Option : MonoBehaviour
     #region Résolution
     private void Start()
     {
-        resolutions = Screen.resolutions;
-        ResolutionDropdown.ClearOptions();
+        resolutions = Screen.resolutions; //Récupère toute les résoluttion possible
+        ResolutionDropdown.ClearOptions();//Enlève les options de bases du Dropdown
 
+
+        //--------Convertie les résolution en String pour les afficher dans le Dropdown---------
         List<string> options = new List<string>();
 
-        int currentResolutionIndex = 0;
+        
         for (int i=0; i<resolutions.Length; i++)
         {
            string option = resolutions[i].width + "x" + resolutions[i].height;
             options.Add(option);
 
-            if(resolutions[i].width == Screen.currentResolution.width &&
+            if(resolutions[i].width == Screen.currentResolution.width && //Cherche la résolution actuelle de l'écran
                resolutions[i].height == Screen.currentResolution.height)
             {
                 currentResolutionIndex = i;
             }
         }
+        //----------------------------------------------------------------------------------------
 
-        ResolutionDropdown.AddOptions(options);
-        ResolutionDropdown.value = currentResolutionIndex;
-        ResolutionDropdown.RefreshShownValue();
+        ResolutionDropdown.AddOptions(options); //Ajoute toutes les résolution possible au Dropdown (en string)
+        ResolutionDropdown.value = currentResolutionIndex; //Assigne la résolution actuelle au Dropdown
+        ResolutionDropdown.RefreshShownValue();//Actualise le Dropdown pour afficher la résolution actuelle
     }
 
-    public void SetResolution(int resolutionIndex)
+    public void SetResolution(int resolutionIndex) //
     {
+        
+        OldResolution = resolutions[currentResolutionIndex];
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        Debug.Log("rr");
+        if (!FirstTime)
+        {
+            StartCoroutine("Validation");
+        }
+        else
+        {
+            FirstTime = false;
+        }
+    }
+
+    public void ResetResolution()
+    {
+        Screen.SetResolution(OldResolution.width, OldResolution.height, Screen.fullScreen);
+        ValidationPanel.SetActive(false);
+    }
+
+    public IEnumerator Validation()
+    {
+        ValidationPanel.SetActive(true);
+        yield return new WaitForSeconds(5);
+
+        ResetResolution();
     }
     #endregion
 
@@ -78,5 +112,6 @@ public class Option : MonoBehaviour
         isAvertissement = IsAverti;
     }
     #endregion
+
 
 }
