2a1919213c09ea5f81583f4d786dc51bd29c2832
Aurélien <cordaurelien@gmail.com>
2021-05-18 02:54:14 +0200

    Update Campagne.cs

--- MythsAndSteel/Assets/MetaGame/Scripts/UI/Menu/Campagne.cs
+++ MythsAndSteel/Assets/MetaGame/Scripts/UI/Menu/Campagne.cs
@@ -1,20 +1,17 @@
-using System.Collections;
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class Campagne : MonoBehaviour
 {
     public MYthsAndSteel_Enum.Scenario _Scenario; //Scénario Séléctionné et affiché
     [SerializeField] int ScenarioVal = 0;
     [SerializeField] int spaceBetweenScenario = 0;
-        
+
     [SerializeField] int Unlocked;//Nombre actuelle de niveau débloqué
 
     [Header("Assignations")]
-  
+
     [SerializeField] private SaveData saveData;
     [SerializeField] private GameObject _buttonLeft = null;
     [SerializeField] private GameObject _buttonRight = null;
@@ -32,99 +29,107 @@ public class Campagne : MonoBehaviour
     [SerializeField] private TextMeshProUGUI BluePlayerVictories;
     [SerializeField] int redPlayerVictories;
     [SerializeField] int bluePlayerVictories;
+
+    private void Start()
+    {
+        PlayerPrefs.SetInt("RethelDone", 0);
+        PlayerPrefs.SetInt("ShanghaiDone", 0);
+        PlayerPrefs.SetInt("StalingradDone", 0);
+        PlayerPrefs.SetInt("HuskyDone", 0);
+        PlayerPrefs.SetInt("GuadalcanalDone", 0);
+        PlayerPrefs.SetInt("ElAlameinDone", 0);
+        PlayerPrefs.SetInt("ElsenbornDone", 0);
+    }
     private void Awake()
     {
         Time.timeScale = 1;
         Unlocked = PlayerPrefs.GetInt("UnlockCampaign");
 
-        if (Unlocked == 0)
+        switch (Unlocked)
         {
-            Jauge0.SetActive(true);
-            Jauge1.SetActive(false);
-            Jauge2.SetActive(false);
-            Jauge3.SetActive(false);
-            Jauge4.SetActive(false);
-            Jauge5.SetActive(false);
-            Jauge6.SetActive(false);
-            Jauge7.SetActive(false);
+            case 0:
+                Jauge0.SetActive(true);
+                Jauge1.SetActive(false);
+                Jauge2.SetActive(false);
+                Jauge3.SetActive(false);
+                Jauge4.SetActive(false);
+                Jauge5.SetActive(false);
+                Jauge6.SetActive(false);
+                Jauge7.SetActive(false);
+                break;
+            case 1:
+                Jauge0.SetActive(false);
+                Jauge1.SetActive(true);
+                Jauge2.SetActive(false);
+                Jauge3.SetActive(false);
+                Jauge4.SetActive(false);
+                Jauge5.SetActive(false);
+                Jauge6.SetActive(false);
+                Jauge7.SetActive(false);
+                break;
+            case 2:
+                Jauge0.SetActive(false);
+                Jauge1.SetActive(false);
+                Jauge2.SetActive(true);
+                Jauge3.SetActive(false);
+                Jauge4.SetActive(false);
+                Jauge5.SetActive(false);
+                Jauge6.SetActive(false);
+                Jauge7.SetActive(false);
+                break;
+            case 3:
+                Jauge0.SetActive(false);
+                Jauge1.SetActive(false);
+                Jauge2.SetActive(false);
+                Jauge3.SetActive(true);
+                Jauge4.SetActive(false);
+                Jauge5.SetActive(false);
+                Jauge6.SetActive(false);
+                Jauge7.SetActive(false);
+                break;
+            case 4:
+                Jauge0.SetActive(false);
+                Jauge1.SetActive(false);
+                Jauge2.SetActive(false);
+                Jauge3.SetActive(false);
+                Jauge4.SetActive(true);
+                Jauge5.SetActive(false);
+                Jauge6.SetActive(false);
+                Jauge7.SetActive(false);
+                break;
+            case 5:
+                Jauge0.SetActive(false);
+                Jauge1.SetActive(false);
+                Jauge2.SetActive(false);
+                Jauge3.SetActive(false);
+                Jauge4.SetActive(false);
+                Jauge5.SetActive(true);
+                Jauge6.SetActive(false);
+                Jauge7.SetActive(false);
+                break;
+            case 6:
+                Jauge0.SetActive(false);
+                Jauge1.SetActive(false);
+                Jauge2.SetActive(false);
+                Jauge3.SetActive(false);
+                Jauge4.SetActive(false);
+                Jauge5.SetActive(false);
+                Jauge6.SetActive(true);
+                Jauge7.SetActive(false);
+                break;
+            default:
+                Jauge0.SetActive(false);
+                Jauge1.SetActive(false);
+                Jauge2.SetActive(false);
+                Jauge3.SetActive(false);
+                Jauge4.SetActive(false);
+                Jauge5.SetActive(false);
+                Jauge6.SetActive(false);
+                Jauge7.SetActive(true);
+                break;
+
         }
-        else if (Unlocked == 1)
-        {
-            Jauge0.SetActive(false);
-            Jauge1.SetActive(true);
-            Jauge2.SetActive(false);
-            Jauge3.SetActive(false);
-            Jauge4.SetActive(false);
-            Jauge5.SetActive(false);
-            Jauge6.SetActive(false);
-            Jauge7.SetActive(false);
-        }
-        else if (Unlocked == 2)
-        {
-            Jauge0.SetActive(false);
-            Jauge1.SetActive(false);
-            Jauge2.SetActive(true);
-            Jauge3.SetActive(false);
-            Jauge4.SetActive(false);
-            Jauge5.SetActive(false);
-            Jauge6.SetActive(false);
-            Jauge7.SetActive(false);
-        }
-        else if (Unlocked == 3)
-        {
-            Jauge0.SetActive(false);
-            Jauge1.SetActive(false);
-            Jauge2.SetActive(false);
-            Jauge3.SetActive(true);
-            Jauge4.SetActive(false);
-            Jauge5.SetActive(false);
-            Jauge6.SetActive(false);
-            Jauge7.SetActive(false);
-        }
-        else if (Unlocked == 4)
-        {
-            Jauge0.SetActive(false);
-            Jauge1.SetActive(false);
-            Jauge2.SetActive(false);
-            Jauge3.SetActive(false);
-            Jauge4.SetActive(true);
-            Jauge5.SetActive(false);
-            Jauge6.SetActive(false);
-            Jauge7.SetActive(false);
-        }
-        else if (Unlocked == 5)
-        {
-            Jauge0.SetActive(false);
-            Jauge1.SetActive(false);
-            Jauge2.SetActive(false);
-            Jauge3.SetActive(false);
-            Jauge4.SetActive(false);
-            Jauge5.SetActive(true);
-            Jauge6.SetActive(false);
-            Jauge7.SetActive(false);
-        }
-        else if (Unlocked == 6)
-        {
-            Jauge0.SetActive(false);
-            Jauge1.SetActive(false);
-            Jauge2.SetActive(false);
-            Jauge3.SetActive(false);
-            Jauge4.SetActive(false);
-            Jauge5.SetActive(false);
-            Jauge6.SetActive(true);
-            Jauge7.SetActive(false);
-        }
-        else if (Unlocked >= 7)
-        {
-            Jauge0.SetActive(false);
-            Jauge1.SetActive(false);
-            Jauge2.SetActive(false);
-            Jauge3.SetActive(false);
-            Jauge4.SetActive(false);
-            Jauge5.SetActive(false);
-            Jauge6.SetActive(false);
-            Jauge7.SetActive(true);
-        }
+
         if (Unlocked == 0)
         {
             _buttonRight.GetComponent<Button>().interactable = false;
@@ -132,12 +137,73 @@ public class Campagne : MonoBehaviour
         }
     }
 
-    private void Update(){
+    private void Update()
+    {
         _mapTransform.GetComponent<RectTransform>().localPosition = Vector2.MoveTowards(_mapTransform.GetComponent<RectTransform>().localPosition, new Vector2(-spaceBetweenScenario * (Screen.width / 1920) * ScenarioVal, _mapTransform.GetComponent<RectTransform>().localPosition.y), Time.deltaTime * _mapSpeed);
         redPlayerVictories = saveData.redPlayerVictories;
         bluePlayerVictories = saveData.bluePlayerVictories;
         RedPlayerVictories.text = redPlayerVictories.ToString();
         BluePlayerVictories.text = bluePlayerVictories.ToString();
+       
+        // CODE A DÉ-COMMENTER SI UN PLATEAU PAR SCENE (+ Réassigner le switch dans le bouton des batailles)
+        /* 
+        if (ScenarioVal == 0 && PlayerPrefs.GetInt("RethelDone") == 1)
+        {
+            _buttonRight.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            _buttonRight.GetComponent<Button>().interactable = false;
+        }
+        if (ScenarioVal == 1 && PlayerPrefs.GetInt("ShanghaiDone") == 1)
+        {
+            _buttonRight.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            _buttonRight.GetComponent<Button>().interactable = false;
+        }
+        if (ScenarioVal == 2 && PlayerPrefs.GetInt("StalingradDone") == 1)
+        {
+            _buttonRight.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            _buttonRight.GetComponent<Button>().interactable = false;
+        }
+        if (ScenarioVal == 3 && PlayerPrefs.GetInt("HuskyDone") == 1)
+        {
+            _buttonRight.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            _buttonRight.GetComponent<Button>().interactable = false;
+        }
+        if (ScenarioVal == 4 && PlayerPrefs.GetInt("GuadalcanalDone") == 1)
+        {
+            _buttonRight.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            _buttonRight.GetComponent<Button>().interactable = false;
+        }
+        if (ScenarioVal == 5 && PlayerPrefs.GetInt("ElAlameinDone") == 1)
+        {
+            _buttonRight.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            _buttonRight.GetComponent<Button>().interactable = false;
+        }
+        if (ScenarioVal == 6 && PlayerPrefs.GetInt("ElsenbornDone") == 1)
+        {
+            _buttonRight.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            _buttonRight.GetComponent<Button>().interactable = false;
+        }
+        */
     }
 
     /// <summary>
@@ -145,33 +211,59 @@ public class Campagne : MonoBehaviour
     /// </summary>
     public void ChangeScene(int sceneID)
     {
-        
-            SceneManager.LoadScene(sceneID);
+        SceneManager.LoadScene(sceneID);
+        switch (sceneID)
+        {
+
+            case 1:
+                PlayerPrefs.SetInt("RethelDone", 1);
+                break;
+            case 2:
+                PlayerPrefs.SetInt("ShanghaiDone", 1);
+                break;
+            case 3:
+                PlayerPrefs.SetInt("StalingradDone", 1);
+                break;
+            case 4:
+                PlayerPrefs.SetInt("HuskyDone", 1);
+                break;
+            case 5:
+                PlayerPrefs.SetInt("GuadalcanalDone", 1);
+                break;
+            case 6:
+                PlayerPrefs.SetInt("ElAlameinDone", 1);
+                break;
+            case 7:
+                PlayerPrefs.SetInt("ElsenbornDone", 1);
+                break;
+            default:
+                break;
+        }
 
     }
 
     /// <summary>
     /// Fonction boutton pour montrer le scénario précédent
     /// </summary>
-    public void Decrease() 
+    public void Decrease()
     {
         int targetValue = ScenarioVal - 1;
 
-        if(targetValue > 0 && targetValue < 7)
+        if (targetValue > 0 && targetValue < 7)
         {
             _buttonRight.GetComponent<Button>().interactable = true;
             _buttonLeft.GetComponent<Button>().interactable = true;
             _Scenario--;
             ScenarioVal--;
         }
-        else if(targetValue == 0)
+        else if (targetValue == 0)
         {
             _buttonRight.GetComponent<Button>().interactable = true;
             _buttonLeft.GetComponent<Button>().interactable = false;
             _Scenario--;
             ScenarioVal--;
         }
-        else if(targetValue < 0) { }
+        else if (targetValue < 0) { }
     }
 
     /// <summary>
@@ -179,30 +271,30 @@ public class Campagne : MonoBehaviour
     /// </summary>
     public void Increase()
     {
-        int targetValue = ScenarioVal+1;
+        int targetValue = ScenarioVal + 1;
 
-        if(targetValue > 0 && targetValue < 6)
+        if (targetValue > 0 && targetValue < 6)
         {
-            _buttonRight.GetComponent<Button>().interactable = true;
             _buttonLeft.GetComponent<Button>().interactable = true;
+            _buttonRight.GetComponent<Button>().interactable = true; // Enlever la ligne quand décommentez la ligne 148 aka CODE A DÉ-COMMENTER SI UN PLATEAU PAR SCENE (+ Réassigner le switch dans le bouton des batailles)
             _Scenario++;
             ScenarioVal++;
         }
-        else if(targetValue == 6)
+        else if (targetValue == 6)
         {
             _buttonRight.GetComponent<Button>().interactable = false;
             _buttonLeft.GetComponent<Button>().interactable = true;
             _Scenario++;
             ScenarioVal++;
         }
-        else if(targetValue > 6){}
+        else if (targetValue > 6) { }
 
-        if(ScenarioVal == targetValue){
-            if(ScenarioVal == Unlocked){
+        if (targetValue == ScenarioVal)
+        {
+            if (ScenarioVal == Unlocked)
+            {
                 _buttonRight.GetComponent<Button>().interactable = false;
             }
         }
     }
-
-
 }
